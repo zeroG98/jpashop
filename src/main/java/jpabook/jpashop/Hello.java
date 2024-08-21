41988d2cf4ff9838fe34952950540de05c6a49d1
sin-eunsu <sin9801@naver.com>
2024-08-21 16:16:32 +0900

    first commit

---
+++ src/main/java/jpabook/jpashop/Hello.java
@@ -0,0 +1,12 @@
+package jpabook.jpashop;
+
+import lombok.Getter;
+import lombok.Setter;
+
+//lombok 적용 확인
+@Getter
+@Setter
+public class Hello {
+
+    private String data;
+}
---
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,40 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    public final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    @Transactional
+    public void updatItem(Long itemId, String name, int price, int stockQuantity) {
+        Item findItem = itemRepository.findOne(itemId);//merge보단 변경감지가 더 좋음
+//        findItem.change(name, price, stockQuantity); //setter보단 entity안에 만들어 놓는게 유지보수에 좋음(추적성)
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+}
